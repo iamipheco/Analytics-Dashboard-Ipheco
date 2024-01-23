833ea5d6a75bc228717e20edbdf100e3466de8d8
Ipheco Chris <iamipheco@gmail.com>
2024-01-23 23:14:10 +0100

    setup for my the analytics dash project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Signup from './components/Signup'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='text-4xl font-bold text-red-950 text-center pt-4'>This is  My React</h1>
+    </div>
+  )
+}
+
+export default App
